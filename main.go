075245937a4d1024c931b0ac8fd3fae9e92631a8
Bidhan <bidhanbaniya789@gamil.com>
2025-02-28 21:03:52 +0545

    ✨ Add efficient digital clock UI with optimized rendering 🚀

--- main.go
+++ main.go
@@ -1,98 +1,139 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
+
+type placeholder [5]string
+
+var (
+	digits = [...]placeholder{
+		{ // 0
+			"███",
+			"█ █",
+			"█ █",
+			"█ █",
+			"███",
+		},
+		{ // 1
+			"██ ",
+			" █ ",
+			" █ ",
+			" █ ",
+			"███",
+		},
+		{ // 2
+			"███",
+			"  █",
+			"███",
+			"█  ",
+			"███",
+		},
+		{ // 3
+			"███",
+			"  █",
+			"███",
+			"  █",
+			"███",
+		},
+		{ // 4
+			"█ █",
+			"█ █",
+			"███",
+			"  █",
+			"  █",
+		},
+		{ // 5
+			"███",
+			"█  ",
+			"███",
+			"  █",
+			"███",
+		},
+		{ // 6
+			"███",
+			"█  ",
+			"███",
+			"█ █",
+			"███",
+		},
+		{ // 7
+			"███",
+			"  █",
+			"  █",
+			"  █",
+			"  █",
+		},
+		{ // 8
+			"███",
+			"█ █",
+			"███",
+			"█ █",
+			"███",
+		},
+		{ // 9
+			"███",
+			"█ █",
+			"███",
+			"  █",
+			"███",
+		},
+	}
+
+	colon = placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+)
 
 func main() {
-	type placeholder [5]string
+	screen.Clear()
 
-	zero := placeholder{
-		"███",
-		"█ █",
-		"█ █",
-		"█ █",
-		"███",
-	}
+	// pre-allocate a builder for each frame
+	var b strings.Builder
 
-	one := placeholder{
-		"██ ",
-		" █ ",
-		" █ ",
-		" █ ",
-		"███",
-	}
+	for {
+		b.Reset()
+		screen.MoveTopLeft()
 
-	two := placeholder{
-		"███",
-		"  █",
-		"███",
-		"█  ",
-		"███",
-	}
+		now := time.Now()
+		h, m, s := now.Hour(), now.Minute(), now.Second()
 
-	three := placeholder{
-		"███",
-		"  █",
-		"███",
-		"  █",
-		"███",
-	}
-
-	four := placeholder{
-		"█ █",
-		"█ █",
-		"███",
-		"  █",
-		"  █",
-	}
-
-	five := placeholder{
-		"███",
-		"█  ",
-		"███",
-		"  █",
-		"███",
-	}
-
-	six := placeholder{
-		"███",
-		"█  ",
-		"███",
-		"█ █",
-		"███",
-	}
-
-	seven := placeholder{
-		"███",
-		"  █",
-		"  █",
-		"  █",
-		"  █",
-	}
-
-	eight := placeholder{
-		"███",
-		"█ █",
-		"███",
-		"█ █",
-		"███",
-	}
-
-	nine := placeholder{
-		"███",
-		"█ █",
-		"███",
-		"  █",
-		"███",
-	}
-
-	digits := [...]placeholder{
-		zero, one, two, three, four, five, six, seven, eight, nine,
-	}
-
-	for line := range digits[0] {
-		for digit := range digits {
-			fmt.Print(digits[digit][line], " ")
+		// Build a slice of the placeholders to display.
+		clocks := []placeholder{
+			digits[h/10], digits[h%10],
+			colon,
+			digits[m/10], digits[m%10],
+			colon,
+			digits[s/10], digits[s%10],
 		}
-		fmt.Println()
+
+		// Toggle the colon on even seconds.
+		showColon := s%2 != 0
+
+		// Build each line of the clock output.
+		for line := 0; line < 5; line++ {
+			for i, ph := range clocks {
+				lineStr := ph[line]
+				if (i == 2 || i == 5) && !showColon {
+					lineStr = "   "
+				}
+				b.WriteString(lineStr)
+				b.WriteString("  ") // gap between placeholders
+			}
+			b.WriteByte('\n')
+		}
+
+		// Print the complete frame.
+		fmt.Print(b.String())
+
+		time.Sleep(time.Second)
 	}
 }
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+REM This script runs the Go UI clock.
+go run main.go
+pause
